4888b50151e09ee33d06c207639a368bf810d143
timothymichaelcook <tmc295@email.vccs.edu>
2022-11-16 10:08:43 -0500

    added to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,17 @@
-// Assignment code here
+//es Variabl
+var characterLength = 16;
+var choiceArray = [];
 
+var specialCharacterArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +22,16 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getPrompts (){
+
+}
+
+//generatePassword
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[randomIndex];
+  }
+  return password;
+}
