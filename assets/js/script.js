538b61fd00941b558d3911e4d193c3d0e43f6b77
timothymichaelcook <tmc295@email.vccs.edu>
2022-12-18 18:28:03 -0500

    finished userInput section asking user what characters they would like in their password and then adding the array to the possible choice array. alerts are working and console.log tests are working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,31 @@
 // Declare/Assign Variables
+var lettersLower = (() => {
+  var caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 97));
+  return caps.concat();
+})();
+
+var lettersUpper = (() => {
+  var caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+  return caps.concat();
+})();
+
+var lettersSpecial = (() => {
+  var caps = [...Array(15)].map((val, i) => String.fromCharCode(i + 33));
+  return caps.concat();
+})();
+
+var numbers = (() => {
+  var caps = [...Array(10)].map((val, i) => String.fromCharCode(i + 48));
+  return caps.concat();
+})();
+
 var generateBtn = document.querySelector("#generate")
-var passwordLength;
+var passwordLength = 128;
 var passwordCharactersArray = [];
 var lowercaseCharactersArray = lettersLower;
 var uppercaseCharactersArray = lettersUpper;
 var specialCharactersArray = lettersSpecial;
 var numbersArray = numbers;
-
-const lettersLower = (() => {
-  const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 97));
-  return caps.concat();
-})();
-
-const lettersUpper = (() => {
-  const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
-  return caps.concat();
-})();
-
-const lettersSpecial = (() => {
-  const caps = [...Array(15)].map((val, i) => String.fromCharCode(i + 33));
-  return caps.concat();
-})();
-
-const numbers = (() => {
-  const caps = [...Array(10)].map((val, i) => String.fromCharCode(i + 48));
-  return caps.concat();
-})();
-
-
-
 
 // Test to see if arrays are generating the correct values
 console.log(lowercaseCharactersArray);
@@ -36,33 +33,52 @@ console.log(uppercaseCharactersArray);
 console.log(specialCharactersArray);
 console.log(numbersArray);
 
-
 // Declare/Assign Functions
-function generatePassword() {
-
-}
-
-
 function userInput() {
+  //Resets Array 
+  passwordCharactersArray = [];
+
   characterLength = parseInt(prompt("Please type your password length? (8 - 128 characters)"))
 
   if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    alert("Please choose a number between 8 -128 characters");
+    alert("Please choose a password length between 8 -128 characters.");
     return false;
   }
+  if(confirm("Would you like your password to include lowercase characters?")) {
+    passwordCharactersArray = passwordCharactersArray.concat(lowercaseCharactersArray);
+  }
+  if(confirm("Would you like your password to include lowercase characters?")) {
+    passwordCharactersArray = passwordCharactersArray.concat(uppercaseCharactersArray);
+  }
+  if(confirm("Would you like your password to include special characters?")) {
+    passwordCharactersArray = passwordCharactersArray.concat(specialCharactersArray);
+  }
+  if(confirm("Would you like your password to include numbers?")) {
+    passwordCharactersArray = passwordCharactersArray.concat(numbersArray);
+  }
+  return true;
 }
 
 
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < passwordLength.length, i++) {
+  
+  }
+}
 
 // Add Event Listener to Generate Button
 generateBtn.addEventListener("click", writePassword);
 
-
 // Write Password to the #password Input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("password");
-  passwordText.value = password;
+  var completeInput = userInput();
+  if (completeInput) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("password");
+    passwordText.value = password;
+  }
+  
 }
 
 
